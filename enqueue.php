858b230bb8d8acf669f8fef107da3ecde918b633
Valentina <valentina.rachiele@gmail.com>
2020-05-08 14:12:31 +0200

    Create enqueue.php

---
+++ enqueue.php
@@ -0,0 +1,2 @@
+//da inserire nel file functions.php, modificando "jost" e il inserendo il link al CSS del font scelto
+wp_enqueue_style( 'jost', 'https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,400;0,700;1,400;1,700&display=swap' ); 
